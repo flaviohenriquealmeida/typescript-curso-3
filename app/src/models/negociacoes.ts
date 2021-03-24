f4398d11cf4a7b0fd5adf9d387ce9e0f985242a0
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-24 10:50:05 +0000

    first commit

---
+++ app/src/models/negociacoes.ts
@@ -0,0 +1,13 @@
+import { Negociacao } from './negociacao.js';
+
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
+
+    public adiciona(negociacao: Negociacao) {
+        this.negociacoes.push(negociacao);
+    }
+
+    public lista(): readonly Negociacao[] {
+        return this.negociacoes;
+    }
+}
