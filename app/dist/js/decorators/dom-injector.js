772f5a419db4b1bc03c225a2ecd8da0c9c832b7b
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-25 13:06:30 +0000

    video3.6

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -2,10 +2,13 @@ export function domInjector(seletor) {
     return function (target, propertyKey) {
         console.log(`Modificando protype ${target.constructor.name}
              e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor 
-                ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o seletor 
+                 ${seletor} para injetar em ${propertyKey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,11 +1,18 @@
 export function domInjector(seletor: string) {
+    
     return function(target: any, propertyKey: string) {
         console.log(`Modificando protype ${target.constructor.name}
              e adicionando getter para a propriedade ${propertyKey}`);
+
+        let elemento: HTMLElement;
+
         const getter = function() {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOM com o seletor 
-                ${seletor} para injetar em ${propertyKey}`);
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+                console.log(`buscando elemento do DOM com o seletor 
+                 ${seletor} para injetar em ${propertyKey}`);
+            }
+            
             return elemento;
         }
 
