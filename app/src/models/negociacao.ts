0377007c3e56dba798f4fdae8c4978900df9888f
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2021-03-25 17:11:21 +0000

    video5.7

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,7 +1,6 @@
-import { Comparavel } from '../interfaces/comparavel.js';
-import { Imprimivel } from '../utils/imprimivel.js';
+import { Modelo } from '../interfaces/modelo.js';
 
-export class Negociacao implements Imprimivel, Comparavel<Negociacao> {
+export class Negociacao implements Modelo<Negociacao> {
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,6 @@
-import { Comparavel } from '../interfaces/comparavel.js';
-import { Imprimivel } from '../utils/imprimivel.js';
+import { Modelo } from '../interfaces/modelo.js';
 import { Negociacao } from './negociacao.js';
-export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
+export class Negociacoes implements Modelo<Negociacoes> {
 
     private negociacoes: Negociacao[] = [];
 
